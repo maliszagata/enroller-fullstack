694a04eebbfe171cb0299c2f70e75674a60efa4a
agata_m <agata.malisz2@gmail.com>
2022-06-17 13:30:00 +0200

    refactor

--- src/main/java/com/company/enroller/App.java
+++ src/main/java/com/company/enroller/App.java
@@ -1,8 +1,10 @@
 package com.company.enroller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
